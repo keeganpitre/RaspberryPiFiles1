a267a253a71f19de3a8b33d044fffdc9f37cb4f2
keeganpitre <keeganpitre@hotmail.com>
2016-12-04 18:55:07 -0500

    Added Sockets

---
+++ raspberryPi_code_arduinoR2.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO ## Import GPIO library
+import time ## Import 'time' library. Allows us to use 'sleep'
+import serial
+import socket
+
+HOST = '192.168.1.136'
+PORT = 12345
+
+RxSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+TxSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('Socket Created')
+
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(32, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+
+ser = serial.Serial('/dev/ttyUSB0',9600, timeout=0, writeTimeout=0)
+print("The code is running!")
+
+try:
+	RxScok.bind((HOST,PORT))
+except socker.error:
+	print("Bind Failed")
+
+try:
+	TxScok.bind((HOST,PORT))
+except socker.error:
+	print("Bind Failed")
+
+RxSock.listen(1)
+print 'Socket awaiting messages'
+(conn,addr) = RxSock.accept()
+print 'Connected'
+
+z=0
+
+
+while z==0:
+    data = conn.recv(1024)
+    print("This is the data " + str(data))
+    ser.write(data)
+
+
+conn.close()
